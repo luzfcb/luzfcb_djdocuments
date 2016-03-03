fec54a942de64658f11b78dd8f0baea5cc4cdcb5
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-03-03 17:48:10 -0300

    adicionado configuracao da pasta de arquivos estaticos, para fazer Pycharm enxerga-los

--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 
 FONT_DIR = os.path.join(
     os.path.dirname(BASE_DIR), 'tests', 'test_project', 'contrib', 'fonts')
