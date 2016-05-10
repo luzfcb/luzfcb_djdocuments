33af19a97432ca2471a709f7d135ac8e17c751ef
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-05-10 11:44:11 -0300

    criacao_banco

--- create_database.sh
+++ create_database.sh
@@ -1,5 +1,8 @@
 #!/bin/env bash
 
 BUILD_N="$(echo $TRAVIS_JOB_NUMBER | tr '.' '_')"
-
 psql -c "create database testdatabase_$BUILD_N;" -U postgres
+export DATABASE_URL="postgres://postgres:@localhost:5432/testdatabase_$BUILD_N"
+echo "DATABASE_URL=${DATABASE_URL}"
+
+
