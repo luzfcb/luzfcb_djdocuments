1136bbcc22a9b987b93e531eea04c66adaebec03
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2017-09-14 11:02:07 -0300

    mostrar modelos em que grupo_dono=None

--- src/djdocuments/managers.py
+++ src/djdocuments/managers.py
@@ -56,7 +56,9 @@ class DocumentoQuerySet(SoftDeletableQuerySet):
                 % self.__class__.__name__
             )
         if grupos_ids and isinstance(grupos_ids, (list, tuple, ValuesListQuerySet)):
-            q &= Q(grupo_dono__in=grupos_ids)
+            q &= (Q(grupo_dono__in=grupos_ids) | Q(grupo_dono=None))
+        else:
+            q &= Q(grupo_dono=None)
         qs = self.filter(q)
         return qs
 
